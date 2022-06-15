5f82bbd8c1f69f7417f2bee9d150e1c52a76cd60
kihiuFrank <franklinekihiu@gmail.com>
2022-06-15 22:05:13 +0300

    update

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,9 @@
 import Web3 from 'web3';
-
-const web3 = new Web3(window.ethereum);
+    
 window.ethereum.enable();
+
+//const web3 = new Web3(window.web3.currentProvider); (deprecated)
+const web3 = new Web3(window.ethereum);
 
 
 export default web3;
